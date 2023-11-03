4d53a26c3b74c056861d3e311563fab670a4dbfd
djoni-pyt <ev561740@gmail.com>
2023-11-03 17:42:09 +0200

    8urok

---
+++ urok8.py
@@ -0,0 +1,57 @@
+# func def
+
+# def my_func():
+#     print("hello")
+
+# my_func()
+
+
+# def my_func(ПАРАМЕТР):
+#     print("hello")
+    
+# my_func(АРГУМЕНТ)
+
+
+# def my_func(ar):
+#     print(ar * 5)
+    
+# my_func(10)
+
+
+# def my_func(*args):
+#     print(args)
+    
+# my_func(1,2,3,4,5,6,7,)
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# locals(), globals() локальные и глобальные переменные 
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# dz
+# 1
+# Написать функцию, которая в качестве аргумента принимает радиус круга. Радиус вводится пользователем с клавиатуры.
+# Функция считает площадь круга.
+# import math 
+
+# q = int(input("радиус круга: "))
+# def my_func():
+#     s = math.pi * q * 2 
+#     return round(s, 2)
+
+# print(f"Площадь окружности с радиусом {q} равна:{my_func()}")
+
+# 2
+
+# def my_funFactorial(num):
+#     i = 1
+#     fc = 1
+#     while i <= num:
+#         fc *= i
+#         i += 1
+#     return print(f"Факториал числа {num} равен {fc}")
+
+
+# n = int(input("Целое число:"))
+# my_funFactorial(n)
+
+# 
